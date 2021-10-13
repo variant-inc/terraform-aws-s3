9f9c7ddd13dabc2009d8ecbf9e81558ff8aa337e
Ralph Cheng <ralphc@drivevariant.com>
2021-10-13 10:10:36 -0400

    [CLOUD-911] static list

--- s3/main.tf
+++ s3/main.tf
@@ -61,7 +61,7 @@ resource "aws_s3_bucket_public_access_block" "bucket" {
 }
 
 resource "aws_s3_bucket_policy" "bucket" {
-  depends_on = ["aws_s3_bucket_public_access_block.bucket"]
+  depends_on = [aws_s3_bucket_public_access_block.bucket]
   bucket = aws_s3_bucket.bucket.id
 
   # Terraform's "jsonencode" function converts a
--- s3/version.tf
+++ s3/version.tf
@@ -1,5 +1,5 @@
 terraform {
   required_version = ">= 1.0.0"
-  backend "local" {
+  backend "s3" {
   }
 }
